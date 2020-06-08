1c6c0bd57b2debeb2ef35a55fc68fb057bfe5fd1
serginho6x <serginho_6x@hotmail.com>
2020-06-08 10:27:24 -0400

    firts commit:

---
+++ src/main/java/com/spring/rest/FacturadoraApplication.java
@@ -0,0 +1,13 @@
+package com.spring.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturadoraApplication.class, args);
+	}
+
+}
